7c09359a6a647091c403b6b7d1f124d400d8c6eb
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2020-04-02 16:35:31 +0100

    Remove unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,59 +29,9 @@
 </template>
 
 <script>
-import web3 from '../util/getWeb3'
 
 export default {
   name: 'home',
-  data() {
-    return {
-      nurseries: [],
-      plants: [],
-      states: [
-        { value: 0, text: "Planted"},
-        { value: 1, text: "Harvested"},
-        { value: 2, text: "Weighted"}
-      ],
-      show: true
-    }
-  },
-  mounted() {
-    web3.eth.getAccounts().then((acc) => {
-      this.supplyContract.deployed().then((contract) => {
-        contract.getNurseryCount().then((count) => {
-          for (var i = 0; i < count; i++) {
-            contract.getNursery(i).then((nursery) => {
-              this.addNursery(nursery);
-            })
-          }
-        })
-      })
-    })
-  },
-  methods: {
-    addPlant: function(plant) {
-      let p = {
-        id: plant[0].toNumber(),
-        state: plant[1].toNumber(),
-        plantedDate: new Date(plant[2].toNumber() * 1000),
-        lat: plant[3].toNumber(),
-        long: plant[4].toNumber(),
-        nursery: plant[5].toString()
-      }
-
-      this.plants.push(p)
-    },
-    addNursery: function(nursery) {
-      let n = {
-        id: nursery[0].toNumber(),
-        name: nursery[1].toString(),
-        lat: nursery[2].toNumber(),
-        long: nursery[3].toNumber()
-      }
-
-      this.nurseries.push(n);
-    },
-  }
 }
 </script>
 
